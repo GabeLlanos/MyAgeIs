eddce7561d953e67f58e6fdf558fed3b0af3ea1f
GabeLlanos <gabriel.llanos@patelhs.org>
2020-10-19 14:00:00 +0000

    Added myAge ++

--- Main.java
+++ Main.java
@@ -1,15 +1,18 @@
 class Main {
   public static void main(String[] args) {
-  int myAge = 15
-  int birthDate = 517
-  int todaysDate = 921
+  int myAge = 15;
+  int birthDate = 517;
+  int todaysDate = 921;
 
- if(age != 15 )
+ if( todaysDate != 517)
  { 
- System.out.println("I am still" + myAge + "years old");
-   
-   if (age == 15)
-   System.out.println("I am " + myAge + "years old");
+System.out.println("I am still " + myAge + " years old ");
+  }
+  else
+  {
+  myAge ++;
+   System.out.println("I am " + myAge + " years old ");
+  }
  }
   }
-}
+
